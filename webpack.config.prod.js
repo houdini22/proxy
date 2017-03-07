14f0d526080587655b1280c28a789f5f08dfbc2e
Michal <baniczek@gmail.com>
2017-03-07 15:53:36 +0100

    webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -44,10 +44,10 @@ module.exports = {
                 loaders: ['style-loader', 'css-loader']
             },
             // files
-            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "file" }
         ]
     }
 };
