959397b73759e74daaba412a956f76922f59099f
Michal <baniczek@gmail.com>
2017-03-11 23:42:52 +0100

    promo box

--- app/components/index/PromoBox.js
+++ app/components/index/PromoBox.js
@@ -7,16 +7,23 @@ class PromoBoxComponent extends React.Component {
             <div className="flex-child">
                 <div className="panel panel-warning panel-promo">
                     <div className="panel-heading">
-                        <h3 className="panel-title">Promotion! Get 33% off by registering now!</h3>
+                        <h3 className="panel-title">Get 33% off price by registering right now!</h3>
                     </div>
                     <div className="panel-body">
                         <div className="row">
-                            <div className="pull-left left-column">
-                                <i className="fa fa-percent"/>
+                            <div className="row text-center percent-row">
+                                <span className="text-success">-33%</span>
                             </div>
-                            <div className="pull-right right-column">
-                                <p>Soon we will provide an export feature with daily email notifications.</p>
-                                <p>Register now to get 33% off for first 3 months. It's one month for free!</p>
+                            <div className="row text-row">
+                                <p>Soon we will provide a premium features such as:</p>
+                                <ul>
+                                    <li>No limit of pages</li>
+                                    <li>No limit of filters</li>
+                                    <li>Daily email notifications with last day online servers</li>
+                                    <li>Export any table to *.txt or CSV format!</li>
+                                </ul>
+                                <p>Register right now to get 33% off for first 3 months.</p>
+                                <p><strong>It's one month for free!</strong></p>
                                 <p className="btn-container">
                                     <Link to="/register" className="btn btn-xs btn-primary">Register now</Link>
                                 </p>
