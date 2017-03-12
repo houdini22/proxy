6d15c88eb9359d7fc4d23b065fbae44cb22a5004
Michal <baniczek@gmail.com>
2017-03-12 18:05:24 +0100

    fix

--- laravel/app/Mail/AccountConfirmed.php
+++ laravel/app/Mail/AccountConfirmed.php
@@ -28,6 +28,6 @@ class AccountConfirmed extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.account_confirmed');
     }
 }
