054e77c549599fe8dc6f3e145453f160d3361db8
Michal <baniczek@gmail.com>
2017-03-15 15:53:23 +0100

    proxydb harvester

--- laravel/app/Proxy/Service/ProxyDB.php
+++ laravel/app/Proxy/Service/ProxyDB.php
@@ -9,17 +9,11 @@ class ProxyDB extends \App\Proxy\Service\AbstractService implements \App\Proxy\S
     public function process()
     {
         $url = $this->config['url'];
-        $start = 0;
-        $end = 10000 / 50;
 
-        for($i = 0; $i <= $end; $i += 50) {
+        for($i = 0; $i <= 10000; $i += 50) {
             $page = $this->downloadPage($url);
-            $phpquery = $this->createPhpQuery($page);
             $addresses = $this->fetchAddresses($page);
             $this->addServers($addresses);
-            $btn = $phpquery->find('form.mb-3 a.btn:last');
-            $btn = pq($btn);
-
             $url = 'https://proxydb.net/?offset=' . $i;
         }
     }
