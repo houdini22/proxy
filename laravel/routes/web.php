5d771ded1e798431843f17e245da4fe959987994
Michal <baniczek@gmail.com>
2017-03-22 03:21:27 +0100

    ...

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -60,7 +60,7 @@ $checkAnonymity = function () {
             break;
         }
     }
-    if($level === FALSE) {
+    if ($level === FALSE) {
         $level = 'elite';
     }
     return $level;
@@ -133,48 +133,23 @@ Route::get('/proxy_test_old', function (Request $request) use ($checkAnonymity) 
     echo '::proxy_test';
 });
 
-Route::get('/proxy_test_http_online', function (Request $request) use($checkAnonymity) {
-    $ping = microtime(true) - (float)$request->query('start');
-    $oldServer = \App\AvailableServer::where('address', '=', $request->query('ip') . ':' . $request->query('port'))->first();
-    $json = [];
-
-    if ($oldServer) {
-        $oldServer->is_available = 1;
-        $oldServer->last_availability = date('Y-m-d H:i:s');
-        $oldServer->ping_error -= 1;
-        $oldServer->ping_success += 1;
-        $oldServer->is_checked_speed = 0;
-        $oldServer->type = $checkAnonymity();
-        $oldServer->ping = $ping;
-        $oldServer->ping_sum += $oldServer->ping;
-        $oldServer->save();
-
-        $json['id'] = $oldServer->id;
-        $json['address'] = $oldServer->address;
-    }
-
-    echo 'proxy_test::';
-    echo json_encode($json);
-    echo '::proxy_test';
-});
-
-Route::get('/proxy_test_socks', function (Request $request) {
+Route::get('/proxy_test_http_online', function (Request $request) use ($checkAnonymity) {
     $ping = microtime(true) - (float)$request->query('start');
     $server = \App\AvailableServer::where('address', '=', $request->query('ip') . ':' . $request->query('port'))->first();
-    $json = array('id' => NULL);
+    $json = [];
 
     if ($server) {
-        $server->ping_socks_error -= 1;
-        $server->ping_socks_success += 1;
-        $server->is_available = $server->was_available = 1;
-        $server->ping = $ping;
+        $server->is_available = 1;
         $server->last_availability = date('Y-m-d H:i:s');
+        $server->ping_error -= 1;
+        $server->ping_success += 1;
         $server->is_checked_speed = 0;
+        $server->type = $checkAnonymity();
+        $server->ping = $ping;
         $server->ping_sum += $server->ping;
-        $server->speed = NULL;
         $server->save();
 
-        if(!$server->country) {
+        if (!$server->country) {
             $client = new \Guzzle\Http\Client('http://freegeoip.net/');
             $request = $client->get("/json/" . $request->query('ip'), array(), array(
                 'timeout' => 10,
@@ -199,23 +174,74 @@ Route::get('/proxy_test_socks', function (Request $request) {
                 $server->timezone = !empty($result['time_zone']) ? $result['time_zone'] : NULL;
                 $server->save();
             }
+
+            $json['id'] = $server->id;
+            $json['address'] = $server->address;
         }
 
-        $json['id'] = $server->id;
-        $json['address'] = $server->address;
-    }
-
-    echo 'proxy_test::';
-    echo json_encode($json);
-    echo '::proxy_test';
-});
-
-Route::group(['middleware' => 'secure'], function () {
-    Route::get('/', function () {
-        return view('welcome');
+        echo 'proxy_test::';
+        echo json_encode($json);
+        echo '::proxy_test';
     });
 
-    Route::any('{path}', function () {
-        return view('welcome');
-    })->where('path', '[a-z\_\/]+')->name('any');
-});
+    Route::get('/proxy_test_socks', function (Request $request) {
+        $ping = microtime(true) - (float)$request->query('start');
+        $server = \App\AvailableServer::where('address', '=', $request->query('ip') . ':' . $request->query('port'))->first();
+        $json = array('id' => NULL);
+
+        if ($server) {
+            $server->ping_socks_error -= 1;
+            $server->ping_socks_success += 1;
+            $server->is_available = $server->was_available = 1;
+            $server->ping = $ping;
+            $server->last_availability = date('Y-m-d H:i:s');
+            $server->is_checked_speed = 0;
+            $server->ping_sum += $server->ping;
+            $server->speed = NULL;
+            $server->save();
+
+            if (!$server->country) {
+                $client = new \Guzzle\Http\Client('http://freegeoip.net/');
+                $request = $client->get("/json/" . $request->query('ip'), array(), array(
+                    'timeout' => 10,
+                    'connect_timeout' => 2
+                ));
+
+                try {
+                    $response = $request->send();
+                    $body = (string)$response->getBody();
+                    $result = json_decode($body, true);
+                } catch (\Exception $e) {
+                    $result = FALSE;
+                }
+                if ($result !== FALSE) {
+                    $server->country = !empty($result['country_name']) ? $result['country_name'] : NULL;
+                    $server->country_code = !empty($result['country_code']) ? $result['country_code'] : NULL;
+                    $server->region_name = !empty($result['region_name']) ? $result['region_name'] : NULL;
+                    $server->city = !empty($result['city']) ? $result['city'] : NULL;
+                    $server->zip = !empty($result['zip_code']) ? $result['zip_code'] : NULL;
+                    $server->lat = !empty($result['latitude']) ? $result['latitude'] : NULL;
+                    $server->lon = !empty($result['longitude']) ? $result['longitude'] : NULL;
+                    $server->timezone = !empty($result['time_zone']) ? $result['time_zone'] : NULL;
+                    $server->save();
+                }
+            }
+
+            $json['id'] = $server->id;
+            $json['address'] = $server->address;
+        }
+
+        echo 'proxy_test::';
+        echo json_encode($json);
+        echo '::proxy_test';
+    });
+
+    Route::group(['middleware' => 'secure'], function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+        Route::any('{path}', function () {
+            return view('welcome');
+        })->where('path', '[a-z\_\/]+')->name('any');
+    });
